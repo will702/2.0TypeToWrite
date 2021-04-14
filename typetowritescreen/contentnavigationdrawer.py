f6534fc4542e98aae666608d9f72250d4e09392d
will702 <67593974+will702@users.noreply.github.com>
2021-04-14 21:16:08 +0700

    Delete typetowritescreen directory

--- typetowritescreen/contentnavigationdrawer.py
+++
@@ -1,8 +0,0 @@
-from kivymd.uix.list import OneLineIconListItem#Importing OneLineIConList
-
-from kivy.uix.boxlayout import BoxLayout
-
-class ContentNavigationDrawer(BoxLayout):#Creating Navigation Drawer
-    pass
-class ItemDrawer(OneLineIconListItem):#Put Item In Drawer
-    pass
